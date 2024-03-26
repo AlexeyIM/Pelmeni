1141296eb54c020cb39775566d64316e0173ede9
AlexeyIM <alfimovalex14@gmail.com>
2024-03-26 18:01:58 +0300

    123

--- Pelmeni.cpp
+++ Pelmeni.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main()
 {
     setlocale(0, "");
-    int p, t, at, qp, qt, qf;
+    int p, t, at, qp, qt, qf, Qp, np;
     cout << "Введите \n";
     cin >> p >> t >> at >> qp >> qt >> qf;
+    Qp = p / (2 * t);
+    np = Qp / qp;
+    ///
 }
